0604ab4921efb11ab516d36e3b3352e47b66d910
RobertBrCM <robertbr.clsmt@outlook.com>
2018-11-24 12:34:37 -0500

    Creación del Proyecto

---
+++ BS.SOLGAS/BS.SOLGAS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BS.SOLGAS.Startup))]
+namespace BS.SOLGAS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
